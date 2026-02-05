4cbcbcd6a706fc823326ec687b3714ee90e6d29e
Parth Rohit <parthrohit@Parths-Air.communityfibre.co.uk>
2026-02-05 17:30:48 +0000

    Add apply link fallback logic

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -1,4 +1,10 @@
 def normalize_job(job):
+    apply_link = (
+        job.get("job_apply_link")
+        or job.get("job_google_link")
+        or job.get("employer_website")
+    )
+
     return {
         "id": job.get("job_id"),
         "title": job.get("job_title"),
@@ -6,5 +12,5 @@ def normalize_job(job):
         "location": job.get("job_city"),
         "employment_type": job.get("job_employment_type"),
         "remote": job.get("job_is_remote", False),
-        "apply_link": job.get("job_apply_link")
+        "apply_link": apply_link
     }

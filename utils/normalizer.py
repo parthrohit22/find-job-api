cb3955d1f2ac0f478a828b9505aeb7f3d5e18db0
Parth Rohit <parthrohit@Parths-Air.communityfibre.co.uk>
2026-02-05 17:36:20 +0000

    Enrich job apply metadata (source and type)

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -5,6 +5,17 @@ def normalize_job(job):
         or job.get("employer_website")
     )
 
+    source = None
+    if apply_link:
+        if "linkedin.com" in apply_link:
+            source = "LinkedIn"
+        elif "indeed.com" in apply_link:
+            source = "Indeed"
+        elif "google.com" in apply_link:
+            source = "Google Jobs"
+        else:
+            source = "Company Site"
+
     return {
         "id": job.get("job_id"),
         "title": job.get("job_title"),
@@ -12,5 +23,7 @@ def normalize_job(job):
         "location": job.get("job_city"),
         "employment_type": job.get("job_employment_type"),
         "remote": job.get("job_is_remote", False),
-        "apply_link": apply_link
+        "apply_link": apply_link,
+        "apply_source": source,
+        "apply_type": "direct" if job.get("job_apply_link") else "redirect"
     }
